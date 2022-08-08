d44b99707a36775743f6de0a1f0f566d20239c01
GameMaster <businesswitharmando@gmail.com>
2022-08-08 13:10:45 -0500

    Add previous files of awesome books

--- README.md
+++ README.md
@@ -1,1 +1,83 @@
-# awesome-books-es6
+![](https://img.shields.io/badge/Microverse-blueviolet)
+# Conference page - HTML/CSS & JavaScript project
+
+> Hi there! This project is based on an online website for a conference. The objectives are:
+- Use semantic HTML tags.
+- Apply best practices in HTML code.
+- Use CSS selectors correctly.
+- Use CSS box model.
+- Use Flexbox to place elements in the page.
+- Demonstrate ability to create UIs adaptable to different screen sizes using media queries.
+- Use GitHub Pages to deploy web pages.
+- Apply JavaScript best practices and language style guides in code.
+- Use JavaScript to manipulate DOM elements.
+- Use JavaScript events.
+- Use objects to store and access data.
+
+## Built With
+
+- HTML
+- CSS
+- JAVASCRIPT
+- (Used Lighthouse, Webhint, and Stylelint as linters)
+
+## Live Demo
+
+You can check a live working version of this project here: [live-demo](https://armandocomellas1.github.io/capstone-project-conference/)
+
+## Live video showing the project
+
+You can check a live working version of this project here: [live-demo](https://drive.google.com/file/d/1Y7vXFLuFu1SEfDlnbkkC3J1IbYIUPWwm/view?usp=sharing)
+
+## Getting Started
+
+To get the content of this project locally you need to run the following commands in your terminal.
+
+### Prerequisites
+- IDE(code editor) like: **Vscode**, **Sublime**, etc.
+- [Git](https://www.linode.com/docs/guides/how-to-install-git-on-linux-mac-and-windows/)
+
+### Setup
+- Clone the repository
+- Change directory into the project folder
+- Install dependencies
+- Open project with text editor to view changes
+- Run the code using [live server](https://www.google.com/search?client=safari&rls=en&q=live+server&ie=UTF-8&oe=UTF-8)
+
+### Install
+- Feel free to clone the repository at this URL: https://github.com/robinbenitezmora/Awesome_Books
+
+### Usage
+- Make changes to javascript and html files then submit pull requests
+
+### Run tests
+- You can use a live server to run the tests
+
+### Deployment
+- Run the project in your local browser to view the index.html
+
+## Authors
+
+👤 **Armando Gabriel Comellas**
+
+- GitHub: [@armandocomellas1](https://github.com/armandocomellas1)
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](https://armandocomellas1.github.io/capstone-project-conference/issues)
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- Thanks to [Cindy Shin](https://www.behance.net/adagio07) for the amazing work she did in the [CC Global Summit 2015 Project](https://www.behance.net/gallery/29845175/CC-Global-Summit-2015), which was used as a reference and guide for this webpage.
+
+- Thanks to my learning partners for their help and support in our learning path:
+
+## 📝 License
+
+This project is [MIT](./MIT.md) licensed.
---
+++ index.js
@@ -0,0 +1,77 @@
+import { InterfaceUser } from './modules/display.js';
+import { Library } from './modules/Library.js';
+import { loadColor } from './modules/color.js';
+import { DateTime } from './modules/luxon.js';
+
+document.addEventListener('DOMContentLoaded', InterfaceUser.displayBooks);
+
+document.getElementById('books__add').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  if (title === '' || author === '') {
+    alert('Please fill in all fields');
+  } else {
+    const countElemt = document.getElementsByTagName('section').length - 1;
+    const book = new Library(title, author, countElemt);
+    InterfaceUser.addBookToLibrary(book, countElemt);
+    const getDataLocalStorage = localStorage.getItem('List');
+    if (getDataLocalStorage !== null) {
+      const validationStorage = JSON.parse(localStorage.getItem('List'));
+      localStorage.setItem('List', JSON.stringify(validationStorage.concat(book)));
+    } else {
+      const arrSetItem = [book];
+      localStorage.setItem('List', JSON.stringify(arrSetItem));
+    }
+
+    InterfaceUser.clearFields();
+    loadColor();
+  }
+});
+
+document.getElementById('book_list').addEventListener('click', (e) => {
+  InterfaceUser.deleteBook(e.target);
+});
+
+document.getElementById('add').addEventListener('click', loadColor);
+document.addEventListener('DOMContentLoaded', loadColor);
+
+// Library with Navigation
+const loadList = document.getElementById('list');
+loadList.addEventListener('click', () => {
+  document.querySelector('.contact').style.display = 'none';
+  document.getElementById('books__add').style.display = 'none';
+  document.getElementById('book_list').style.display = 'block';
+  document.getElementsByClassName('title_list')[0].style.display = 'flex';
+  document.getElementsByClassName('unique')[0].style.display = 'flex';
+  document.getElementsByClassName('title_add_book')[0].style.display = 'none';
+  document.getElementsByClassName('title_footer')[0].style.display = 'none';
+});
+
+const addBook = document.getElementById('add-book');
+addBook.addEventListener('click', () => {
+  document.querySelector('.contact').style.display = 'none';
+  document.getElementById('books__add').style.display = 'flex';
+  document.getElementById('book_list').style.display = 'none';
+  document.getElementsByClassName('title_list')[0].style.display = 'none';
+  document.getElementsByClassName('unique')[0].style.display = 'none';
+  document.getElementsByClassName('title_add_book')[0].style.display = 'flex';
+  document.getElementsByClassName('title_footer')[0].style.display = 'none';
+});
+
+const loadContact = document.getElementById('contact');
+loadContact.addEventListener('click', () => {
+  document.querySelector('.contact').style.display = 'flex';
+  document.getElementById('books__add').style.display = 'none';
+  document.getElementById('book_list').style.display = 'none';
+  document.getElementsByClassName('title_add_book')[0].style.display = 'none';
+  document.getElementsByClassName('title_list')[0].style.display = 'none';
+  document.getElementsByClassName('unique')[0].style.display = 'none';
+  document.getElementsByClassName('title_footer')[0].style.display = 'flex';
+});
+
+// Date
+const nowDate = DateTime.now();
+const dateS = { month: 'long', day: 'numeric' };
+const showONe = nowDate.setLocale('en-US').toLocaleString(dateS);
+document.getElementById('date').innerHTML = `${showONe} ${nowDate.year}, ${nowDate.c.hour}:${nowDate.c.minute}:${nowDate.c.second}`;
