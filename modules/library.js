e5c07d0b602c638f75f66cca30a4bb2ba9e91757
GameMaster <businesswitharmando@gmail.com>
2022-08-08 14:12:54 -0500

    fixing errors that books are not showing up

--- modules/library.js
+++
@@ -1,7 +0,0 @@
-//Containes contructor method of the books
-function Library(title, author, countElemt) {
-  this.title = title;
-  this.author = author;
-  this.countElemt = `elem_${countElemt}`;
-}
-export { Library };
