ec62731ab45ddf041d10c1f90a99b60470802d81
GameMaster <businesswitharmando@gmail.com>
2022-08-08 14:56:34 -0500

    Fixing files

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 //This is the main index.js which will import the modules
-import { InterfaceUser } from './display.js';
-import { Library } from './Library.js';
-import { loadColor } from './color.js';
-import { DateTime } from './luxon.js';
+import { InterfaceUser } from './modules/display.js';
+import { Library } from './modules/Library.js';
+import { loadColor } from './modules/color.js';
+import { DateTime } from './modules/luxon.js';
 
 document.addEventListener('DOMContentLoaded', InterfaceUser.displayBooks);
 
