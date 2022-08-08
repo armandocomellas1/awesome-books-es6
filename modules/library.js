4563671139574acf97dfbb95531010c8b42b85d6
GameMaster <businesswitharmando@gmail.com>
2022-08-08 15:29:23 -0500

    Fixing files

--- modules/library.js
+++ modules/library.js
@@ -1,5 +1,5 @@
 //Containes contructor method of the books
-export default function Library(title, author, countElemt) {
+export function Library(title, author, countElemt) {
   this.title = title;
   this.author = author;
   this.countElemt = `elem_${countElemt}`;
